f2714cd9dd1703480deb4504b9c0f84c0c1953a5
darshcloud <darshini.vn@gmail.com>
2023-03-04 16:00:45 -0800

    Created Side Navigation Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation';
+import {BrowserRouter} from 'react-router-dom';
 
 
 function App() {
   return (
+    <BrowserRouter>
       <Navigation />
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+import {NavLink} from 'react-router-dom';
 import {Navbar,Nav} from 'react-bootstrap';
 import logo from '../static/logo.png'
 import "../App.css";
-
 
 const Navigation = () => {
   return (
@@ -19,6 +27,26 @@ const Navigation = () => {
             Student Management System
         </Navbar.Brand>
     </Navbar>
+    <div className='sidebar'>
+<CDBSidebar textColor="#333" backgroundColor="#f0f0f0">
+    <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
+      Navigation
+    </CDBSidebarHeader>
+    <CDBSidebarContent>
+      <CDBSidebarMenu>
+        <NavLink exact to="/" activeClassName="activeClicked">
+          <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
+        </NavLink>
+        <NavLink exact to="/student" activeClassName="activeClicked">
+          <CDBSidebarMenuItem icon="list">Students List</CDBSidebarMenuItem>
+        </NavLink>
+        <NavLink exact to="/manage" activeClassName="activeClicked">
+          <CDBSidebarMenuItem icon="user">Manage Students</CDBSidebarMenuItem>
+        </NavLink>
+      </CDBSidebarMenu>
+    </CDBSidebarContent>
+  </CDBSidebar>
+</div>
     </div>
   );
 };
