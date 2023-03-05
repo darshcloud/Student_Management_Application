1bbf662b638239273b4e94d589d854fc9bf2576c
darshcloud <darshini.vn@gmail.com>
2023-03-04 21:41:30 -0800

    Integrated frontend with backend and changes implemented for CRUD operations

--- frontend/src/services/StudentService.js
+++ frontend/src/services/StudentService.js
@@ -4,3 +4,37 @@ export function getStudents() {
   return axios.get('http://127.0.0.1:8000/students/')
     .then(response => response.data)
 }
+
+export function deleteStudent(studentId) {
+  return axios.delete('http://127.0.0.1:8000/students/' + studentId + '/', {
+   method: 'DELETE',
+   headers: {
+     'Accept':'application/json',
+     'Content-Type':'application/json'
+   }
+  })
+  .then(response => response.data)
+}
+
+export function addStudent(student){
+  return axios.post('http://127.0.0.1:8000/students/', {
+    studentId:null,
+    firstName:student.firstName.value,
+    lastName:student.lastName.value,
+    registrationNumber:student.registrationNumber.value,
+    email:student.email.value,
+    course:student.course.value
+  })
+    .then(response=>response.data)
+}
+
+export function updateStudent(stuid, student) {
+  return axios.put('http://127.0.0.1:8000/students/' + stuid + '/', {
+    firstName:student.firstName.value,
+    lastName:student.lastName.value,
+    registrationNumber:student.registrationNumber.value,
+    email:student.email.value,
+    course:student.course.value
+  })
+   .then(response => response.data)
+}
