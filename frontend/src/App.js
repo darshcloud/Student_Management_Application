912b5fd9b80ed2cd792c5504542507c59cf0dcae
darshcloud <darshini.vn@gmail.com>
2023-03-04 20:28:25 -0800

    Created Home Page for Student Management Application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation';
-import {BrowserRouter} from 'react-router-dom';
+import Home from "./components/Home";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
+      <Routes>
+         <Route exact path="/" element={<Home/>} />
+       </Routes>
     </BrowserRouter>
   );
 }
